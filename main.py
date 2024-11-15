a753f72fe44cb98306166b37b0f80b53fd238d85
Dave Mackey <dave@davemackey.net>
2024-11-15 15:10:32 -0500

    Add getting field mappings for index, adding document to index

--- main.py
+++ main.py
@@ -12,22 +12,37 @@ client = OpenSearch(
     http_auth=(user, password),
 )
 
-# Show the available indices
+"""Show the available indices"""
 # indices = client.indices.get_alias().keys()
 # print(f"Available indices: {list(indices)}")
 
-# Query for a document by term
-query = {
-    "size": 1,
-    "query": {
-        "match": {
-            "title": "team"
-        }
-    }
-}
-response = client.search(index=index, body=query)
-print(json.dumps(response, indent=2))
+"""Get the field mappings for an index"""
+# try:
+#     mapping = client.indices.get_mapping(index=index)
+#     print(json.dumps(mapping, indent=2))
+# except Exception as e:
+#     print(f"Error getting mapping: {e}")
 
-# Search the index
+"""Query for a document by term"""
+# query = {
+#     "size": 1,
+#     "query": {
+#         "match": {
+#             "title": "team"
+#         }
+#     }
+# }
+# response = client.search(index=index, body=query)
+# print(json.dumps(response, indent=2))
+
+"""Return all documents"""
 # response = client.search(index=index, body={"query": {"match_all": {}}})
 # print(response)
+
+"""Add a document to the index"""
+document = {
+    "title": "TESTOCP: Testing Adding a Document",
+    "description": "This is a test document to test adding a document to the index."
+}
+response = client.index(index=index, body=document)
+print(response)
